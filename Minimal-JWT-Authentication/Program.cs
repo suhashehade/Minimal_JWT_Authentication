da61315d6fedada384cb2ca52aa6276c8e97700a
Suha Shehadeh <suha.shehadeh@foothillsolutions.com>
2026-08-16 12:26:11 +0300

    feat: Creating the Minimal API

--- Minimal-JWT-Authentication/Program.cs
+++ Minimal-JWT-Authentication/Program.cs
@@ -8,6 +8,8 @@ internal static class  Program
         var app = builder.Build();
        
         app.MapGet("/", () => "Hello World!");
+        
+        app.MapGet("/welcome", () => "Welcome! You are authorized.").RequireAuthorization();
 
         app.Run();
     }

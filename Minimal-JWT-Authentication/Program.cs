54bfb7a3d8473432b76b853dd60b63f449155bc0
Suha Shehadeh <suha.shehadeh@foothillsolutions.com>
2026-08-16 14:18:56 +0300

    test: test API using Postman

--- Minimal-JWT-Authentication/Program.cs
+++ Minimal-JWT-Authentication/Program.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.IdentityModel.Tokens;
 using Minimal_JWT_Authentication.Auth;
 
@@ -11,10 +12,15 @@ internal static class  Program
     {
         var builder = WebApplication.CreateBuilder(args);
        
-        
-        var jwtSettings  = builder.Configuration.GetSection("JwtSettings")
-                                                .Get<JwtSettings>() 
-                           ?? throw new InvalidOperationException();
+        builder.Services.AddScoped<JwtTokenGenerator>();
+        builder.Services.Configure<JwtSettings>(
+            builder.Configuration.GetSection("JwtSettings")
+        );
+
+        var jwtSettings = builder.Configuration
+                              .GetSection("JwtSettings")
+                              .Get<JwtSettings>()
+                          ?? throw new InvalidOperationException();
         
         var signingKey = new SymmetricSecurityKey(
             Encoding.UTF8.GetBytes(jwtSettings.Key)
@@ -44,9 +50,28 @@ internal static class  Program
         app.UseAuthorization();
         
         app.MapGet("/", () => "Hello World!");
+        app.MapPost("/login", (LoginRequest request, JwtTokenGenerator tokenGenerator) =>
+        {
+            if (request.Username != "suha" || request.Password != "123")
+            {
+                return Results.Unauthorized();
+            }
+
+            var token = tokenGenerator.GenerateToken(
+                request.Username,
+                request.Password
+            );
+
+            return Results.Ok(new
+            {
+                token
+            });
+        });
         
         app.MapGet("/welcome", () => "Welcome! You are authorized.").RequireAuthorization();
 
         app.Run();
     }
 }
+
+public record LoginRequest(string Username, string Password);
